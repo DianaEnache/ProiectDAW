b6a098c26dcb40525332553acf03f3ef1b63bb74
DianaEnache <115735875+DianaEnache@users.noreply.github.com>
2024-02-05 01:45:33 +0200

    Proiect
    Shop_Project

---
+++ Shopping_project/Shopping_project/Infrastructure/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping_project.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Shopping_project.Infrastructure
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+        
+            public DbSet<Product> Products {  get; set; }
+            public DbSet<Category> Categories { get; set; }
+
+
+
+
+    }
+
+}
+
